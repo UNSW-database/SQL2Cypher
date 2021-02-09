978683ff51c33f38a4b0f2821c590fa9c9195864
Shunyang Li <shunyang.li@unsw.edu.au>
2021-02-09 17:19:44 +1100

    finish sql parse and need to be test

--- sp.py
+++ sp.py
@@ -15,12 +15,12 @@ from moz_sql_parser import parse
 
 
 storage = [{
-    'src': 't1',
-    'dst': 't2',
-    'src_key': 't3 k1',
-    'dst_key': 't3 k2',
-    'type': 'isLabel',
-    'label': 'relationship label',
+    'src': 'title_basic',
+    'dst': 'title_crew',
+    'src_key': 'tconst',
+    'dst_key': 'tconst',
+    'type': 'no',
+    'label': 'KNOWN',
 }]
 
 
@@ -40,7 +40,9 @@ def parse_from(parses):
         # add the value together
         for kv in parses["from"]:
             tables[kv["name"]] = kv["value"]
-            command += "(" + kv["name"] + ":" + kv["value"] + ") "
+            # command += "(" + kv["name"] + ":" + kv["value"] + ") "
+
+        command += ", ".join("({}:{})".format(kv["name"], kv["value"]) for kv in parses["from"])
     else:
         tables[parses["from"]["name"]] = parses["from"]["value"]
         command += "(" + parses["from"]["name"] + ":" + parses["from"]["value"] + ") "
@@ -55,7 +57,8 @@ def parse_where(parses):
     :return: where condition with cypher query
     """
     # get all the tables as dict format
-    tables = parse_from(parses)
+    tables, command = parse_from(parses)
+    print(tables)
     ope = {
         'neq': '!=',
         'eq': '=',
@@ -65,13 +68,14 @@ def parse_where(parses):
         'lte': '<='
     }
     relation = ""
-    condition = ""
+    condition = " WHERE "
     # record all the visited node in the node graph
     visited = []
 
     # to record all the src and dst node
-    src = [v["src"] for v in storage]
-    dst = [v['dst'] for v in storage]
+    # src = [v["src"] for v in storage]
+    # dst = [v['dst'] for v in storage]
+
     if "where" in parses.keys():
         # where is should be a dict format
         if type(parses["where"]) is not dict:
@@ -89,56 +93,85 @@ def parse_where(parses):
                 # means one condition
                 pass
             elif type(where[op]) is list:
-                # means and or or
-                for val in where[op]:
-                    for k in val.keys():
-                        # means eq or nep something
-                        """
-                        check eq to get whether exists relationship
-                        only this can be relationship
-                        """
-                        if k == "eq":
-                            # get two keys
-                            key1, key2 = val[k][0], val[k][1]
-                            t1, pk1 = key1.split(".")
-                            t2, pk2 = key2.split(".")
+                if op == "and":
+                    # there can be a relationship
+                    """
+                    1. extract eq operations firstly
+                    2. consider the relationships
+                    """
+                    relationships = []
+                    conditions = []
+                    for operations in where[op]:
+                        for key in operations.keys():
+                            # put all the eq together at the first
+                            if key == "eq":
+                                relationships.append(operations[key])
+                            else:
+                                # just combine together for and conditions
+                                conditions.append(" {} ".format(ope[key]).join([str(i) for i in operations[key]]))
 
-                            # check whether relation
-                            origin_table_name1 = tables[t1]
-                            origin_table_name2 = tables[t2]
+                    # now consider the relationships
+                    for re in relationships:
+                        key1, key2 = re[0], re[1]
+                        t1, pk1 = key1.split(".")
+                        t2, pk2 = key2.split(".")
 
-                            # must has a start node, means there is a relationship
-                            # then check whether exist
-                            if origin_table_name1 in src and origin_table_name2 in dst:
+                        # check whether relation
+                        origin_table_name1 = tables[t1]
+                        origin_table_name2 = tables[t2]
+
+                        # consider relationships
+                        for st in storage:
+                            if origin_table_name1 == st["src"] and origin_table_name2 == st["dst"]:
                                 # 1->2
-                                pass
-                            elif origin_table_name2 in src and origin_table_name1 in dst:
+                                relation += ", ({})-[:{}]->({}) ".format(t1, st["label"], t2)
+                            elif origin_table_name2 == st["src"] and origin_table_name1 == st["dst"]:
                                 #  2->1
-                                pass
+                                relation += ", ({})-[:{}]->({}) ".format(t2, st["label"], t1)
+                            elif origin_table_name1 == st["src"] and st["type"] == "isLabel":
+                                # join relationship, there should have a a->b->c relationship
+                                for tr in relationships:
+                                    if tr[0] == key2:
+                                        # means should have a join relationship
+                                        if origin_table_name2 == st["src_key"].split(" ")[0]:
+                                            # add the relationship
+                                            relation += ", ({})-[{}:{}]->({})".\
+                                                format(t1, t2, st["label"], tr[1].split(" ")[0])
+                                            # remove the join conditions
+                                            del tr
+                                        else:
+                                            # can not find correct relationship
+                                            conditions.append("{}={}".format(key1, key2))
+                            elif origin_table_name2 == st["src"] and st["type"] == "isLabel":
+                                # join relationship
+                                # join relationship, there should have a a->b->c relationship
+                                for tr in relationships:
+                                    if tr[0] == key1:
+                                        # means should have a join relationship
+                                        if origin_table_name1 == st["src_key"].split(" ")[0]:
+                                            # add the relationship
+                                            relation += ", ({})-[{}:{}]->({})". \
+                                                format(t2, t1, st["label"], tr[1].split(" ")[0])
+                                            # remove the join conditions
+                                            del tr
+                                        else:
+                                            # can not find correct relationship
+                                            conditions.append("{} = {}".format(key1, key2))
+                            else:
+                                conditions.append("{} = {}".format(key1, key2))
 
-                            # to storage the relationship
-                            # for st in storage:
-                            #     if origin_table_name1 == st["src"] and pk1 == st["src_key"]:
-                            #         if origin_table_name2 == st["dst"] and pk2 == st["dst_key"]:
-                            #             # means relationship 1->2
-                            #             relation += t1 + "-" + "[:{}]".format(st["label"]) + t2
-                            #     elif origin_table_name2 == st["src"] and pk2 == st["src_key"]:
-                            #         if origin_table_name1 == st["dst"] and pk1 == st["dst_key"]:
-                            #             # means relationship 2->1
-                            #             relation += t2 + "-" + "[{}]".format(st["label"]) + t1
+                    condition += " AND ".join(conditions)
+                else:
+                    # there can not be a relationship
+                    conditions = []
+                    for operations in where[op]:
+                        for key in operations.keys():
+                            conditions.append(" {} ".format(ope[key]).join([str(i) for i in operations[key]]))
 
-
-                        else:
-                            # just join the relation
-                            pass
-
-        # means it should be a connectable table
-        if " " in storage["src_key"] and " " in storage["dst_key"]:
-            # check whether exist in the where condition and src_table == dst_table
-            src_table, src_key = storage["src_key"].split(" ")
-            dst_table, dsk_key = storage["dst_key"].split(" ")
-
-    return None
+                    condition += " OR ".join(conditions)
+    print(condition)
+    print(relation)
+    return command + relation + condition
 
 
 def parse_limit(parses):
@@ -148,10 +181,10 @@ def parse_limit(parses):
     :return:
     """
     if "limit" in parses.keys():
-        if not parses["limit"].isdigit():
+        if type(parses["limit"]) is not int:
             raise Exception("SQL limit incorrect")
         else:
-            return "LIMIT " + parses["limit"]
+            return " LIMIT " + str(parses["limit"])
 
     return None
 
@@ -164,6 +197,7 @@ def parse_head(values):
     """
     # means it is select query
     if "select" in values.keys():
+        command = parse_where(values)
         # if it is select then we need to consider whether it include some functions
         if type(values["select"]) is list:
             # means select multiple values, it can be value or functions
@@ -171,10 +205,12 @@ def parse_head(values):
             if any(type(i) != dict for i in values["select"]):
                 raise Exception("Incorrect SQL format")
             # handle the multiple values case
-            return "return " + ", ".join([s["value"] for s in values["select"]]) + ";"
+            command += " return " + ", ".join([s["value"] for s in values["select"]])
         else:
             # means select only one value
-            return "return " + values["select"] + ";"
+            command += " return " + values["select"]
+        command += parse_limit(values)
+        return command
     elif "update" in values.keys():
         # means it is update query, nodes update and rel update
         pass
@@ -184,8 +220,12 @@ def parse_head(values):
 
 
 if __name__ == '__main__':
-    raw = "SELECT a, b FROM title_basic tb, title_rating tr, title_crew tc, title_akas ta WHERE tb.tconst = tc.tconst LIMIT 100000 ;"
+    raw = "SELECT a, b FROM title_basic tb, title_rating tr, title_crew tc, title_akas ta WHERE tb.tconst = tc.tconst " \
+          "AND tc.tconst = ta.tconst AND ta.a >= 1 LIMIT 100000 ;"
     values = parse(raw)
-    print(values)
+    print(parse_head(values))
+    # parse_where(values)
+
+
     # parse_head(values)
 
